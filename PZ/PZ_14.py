01e701f42350a1304eb1809405fcad12af0a282b
FieryTiger <Artemstarcikov9@gmail.com>
2024-03-06 13:42:50 +0300

    Com

--- PZ/PZ_14.py
+++ PZ/PZ_14.py
@@ -4,7 +4,7 @@
 import re
 f1 = open('Dostoevsky.txt', 'r', encoding='utf-8')
 file = f1.read()
-ends = re.findall(r'Достоевск[а-яё]+', file)   # Поиск совпадений в файле
+ends = re.findall(r'\bДостоевск[а-яё]+\b', file)   # Поиск совпадений в файле
 ready = list(set(ends))   # Исключение совпдений
 f1.close()
 print(ready)
